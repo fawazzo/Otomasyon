d43db3cea63b38aaea02260860e21993f45c2f46
fawaz <91321838+fawazzo@users.noreply.github.com>
2024-09-10 13:31:58 +0300

    Delete Otomasyon directory

--- Otomasyon/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Otomasyon.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
